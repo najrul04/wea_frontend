e75889e382ec1654fddf702d57870b3211a84261
MD Najrul Islam Sumon <nazrul171992@gmail.com>
2025-02-01 19:02:54 +0600

    creating-location-maker

---
+++ src/components/LocationMarker/LocationMarker.tsx
@@ -0,0 +1,10 @@
+import {Icon} from '@iconify/react';
+import locationIcon from '@iconify/icons-mdi/fire-alert';
+
+const LocationMarker = () => {
+  return (
+    <div>LocationMarker</div>
+  )
+}
+
+export default LocationMarker
